d693cb9c4bbf06cea417ec454227580cb7785882
Ayesha.Jamshed <ayeshajamshed54@gmail.com>
2025-07-11 18:23:20 +0500

    latest changes

--- cypress/e2e/Admin.cy.js
+++ cypress/e2e/Admin.cy.js
@@ -1,7 +1,14 @@
-describe("Login", () => {
-  it("Login the OrangeHRM System", () => {
+describe("Admin", () => {
+  it("verify the admin page functionality", () => {
+    cy.login();
     cy.visit(
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+      "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers"
     );
+    cy.get(".orangehrm-header-container > .oxd-button").contains("Add").click();
+    cy.get(".oxd-select-text-input")
+      .eq(0)
+      .click()
+      .log("Clicked the User Role dropdown");
+    cy.get(".oxd-select-option").contains("ESS").click();
   });
 });
